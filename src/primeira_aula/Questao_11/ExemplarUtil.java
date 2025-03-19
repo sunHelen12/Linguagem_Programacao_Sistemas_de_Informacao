859530ba4d0f9c2b65434a4b11709b24a2d49428
sunHelen12 <sunrise.developer12@gmail.com>
2025-03-19 19:56:24 -0300

    Atualizações da Lista

---
+++ src/primeira_aula/Questao_11/ExemplarUtil.java
@@ -0,0 +1,7 @@
+package primeira_aula.Questao_11;
+
+public class ExemplarUtil {
+    public static void main(String[] args) {
+        Exemplar e1 = new Exemplar(271166, "Java: como Programar", "Harvey M. Deitel e Paul J. Deitel", false);
+    }
+}
